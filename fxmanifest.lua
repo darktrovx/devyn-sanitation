e272d0135de01f366449d43937d77a60a7916273
Devin Monro <mage1d@yahoo.com>
2022-04-14 10:51:18 -0400

    Remove old PolyZone links

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,16 +3,8 @@ games { 'gta5' }
 
 author 'devyn'
 
-client_scripts {
-    '@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/EntityZone.lua',
-	'@PolyZone/CircleZone.lua',
-	'@PolyZone/ComboZone.lua',
-    "client/*.lua"
-}
-server_scripts {
-    "server/*.lua",
-}
+client_scripts {"client/*.lua"}
+server_scripts {"server/*.lua"}
+
 shared_scripts {'shared/*.lua'}
 lua54 'yes'
